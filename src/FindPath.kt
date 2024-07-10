a10a5fb96219b9320a3bfd809f10bd95ec43765e
Vasily Markov <mvv1993Vasya@gmail.com>
2024-07-10 14:57:34 +0300

    Structure

--- src/FindPath.kt
+++ src/FindPath.kt
@@ -1,7 +1,19 @@
-class CostMatrix {
+import Polygon
 
+class CostMatrix(grid: Grid,
+                 var matrix_size = Grid.obstacles_points.size + Grid.field_points.size):
+Array<DoubleArray>(matrix_size)
+{
+    val emptyArray = arrayOf<DoubleArray>()
+
+    fun calcCost() {
+
+    }
 }
 
-class Path {
+class HamiltonPath(cost_matrix: CostMatrix)  {
 
+    fun findPath(): List<Point> {
+
+    }
 }
--- src/Polygon.kt
+++ src/Polygon.kt
@@ -1,13 +1,66 @@
-open class Polygon(private var points: MutableList<Double>) {
+data class Point(val x: Double, val y: Double)
+
+data class Line(val p1: Point, val p2: Point)
+
+open class Polygon(private var points: List<Point>) {
     override fun toString(): String {
         return points.toString()
     }
 }
 
-class Field(points: MutableList<Double>): Polygon(points) {
+class Field(points: List<Point>): Polygon(points) {
+    init {
+
+    }
+}
+
+class Obstacle(points: List<Point>): Polygon(points) {
+    enum class Category {
+        A, B, C, D, NONE
+    }
+
+    var category = Category.NONE
+
+    init {
+
+    }
+
+    fun categorize() {
+
+    }
 
 }
 
-class Obstacle(points: MutableList<Double>): Polygon(points) {
+class Grid(grid_step: Double, azimut: Double, field: Field, obstacles: List<Obstacle>) {
 
+    val obstacles_points = HashMap<UInt, List<Point>>()
+    val field_points = emptyList<Point>()
+
+    init {
+        generateLines()
+    }
+
+    private fun generateLines() {
+
+    }
+
+    //Get points of intersection and categorize
+    private fun intersection(obs: Obstacle) {
+
+    }
+
+    //Get points of intersection
+    private fun intersection(field: Field) {
+
+    }
+
+    fun generateGlobalPoints(field: Field) {
+        intersection(field)
+    }
+
+    fun generateObstaclesPoints(obstacles: List<Obstacle>) {
+        for (obs in obstacles) {
+            intersection(obs)
+        }
+    }
 }
